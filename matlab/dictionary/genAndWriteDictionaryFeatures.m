dc86cc154e56cd5a28eb8851a56bbe0256cb6d88
John Bogovic <bogovicj@janelia.hhmi.org>
2014-07-11 15:12:12 -0400

    code to write features for sample medulla data

--- matlab/dictionary/genAndWriteDictionaryFeatures.m
+++ matlab/dictionary/genAndWriteDictionaryFeatures.m
@@ -1,4 +1,6 @@
 % genAndWriteDictionaryFeatures
+%
+% dbstop if error; genAndWriteDictionaryFeatures;
 
 % depends on 
 
@@ -20,20 +22,37 @@ clear D_list;
 
 % make sure num clusters is correct
 param.K = size(D,2);
+param.verbose = true;
 
 %%
 
-feats = getTrainTestData( ds.data_fn{1}, ds.labels_fn{1}, ds.mask_fn{1}, ...
-                            N, D, patch_size, param );
-
-success = writeFeaturesH5( feats, [], '/nobackup/saalfeld/john/forPhilipp/ds_18_dict1k.h5' );
+% feats = getTrainTestData( ds.data_fn{1}, ds.labels_fn{1}, ds.mask_fn{1}, ...
+%                            N, D, patch_size, param );
+% 
+% success = writeFeaturesH5( feats, [], '/nobackup/saalfeld/john/forPhilipp/ds_18_dict1k_faster.h5' );
+% success
 
 %%
 
 sample_dir = '/groups/saalfeld/home/bogovicj/dev/dawmr/dawmr_lib_public/projects/sample_medulla/';
-data_fn_samp = fullfile(sample_dir, 'medulla_sub1_data.h5');
+data_fn_samp_trn = fullfile(sample_dir, 'medulla_sub1_data.h5');
+labels_fn_samp_trn = fullfile(sample_dir, 'medulla_sub1_labels_1.h5');
+mask_fn_samp_trn = fullfile(sample_dir, 'medulla_sub1_mask_1.h5');
 
-feats = getTrainTestData( data_fn_samp, [], [], ...
+feats_trn = getTrainTestData( data_fn_samp_trn, labels_fn_samp_trn, mask_fn_samp_trn, ...
                             [], D, patch_size, param );
                         
-success = writeFeaturesH5( feats, [], '/nobackup/saalfeld/john/forPhilipp/ds_tst3_dict1k.h5' );
+success_trn = writeFeaturesH5( feats_trn, [], '/nobackup/saalfeld/john/forPhilipp/med_sample_train_wLabels.h5' );
+success_trn 
+
+%
+sample_dir = '/groups/saalfeld/home/bogovicj/dev/dawmr/dawmr_lib_public/projects/sample_medulla/';
+data_fn_samp_tst = fullfile(sample_dir, 'medulla_sub2_data.h5')
+labels_fn_samp_tst = fullfile(sample_dir, 'medulla_sub2_labels_1.h5')
+mask_fn_samp_tst = fullfile(sample_dir, 'medulla_sub2_mask_1.h5')
+
+feats_tst = getTrainTestData( data_fn_samp_tst, labels_fn_samp_tst, mask_fn_samp_tst, ...
+                            [], D, patch_size, param );
+                        
+success_tst = writeFeaturesH5( feats_tst, [], '/nobackup/saalfeld/john/forPhilipp/med_sample_test_wLabels.h5' );
+success_tst
