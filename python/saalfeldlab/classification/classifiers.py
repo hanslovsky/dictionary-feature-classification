26ede5f63dfdea79913b09be903165f603250ea1
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-07-11 15:35:25 -0400

    added AdaBoost

--- python/saalfeldlab/classification/classifiers.py
+++ python/saalfeldlab/classification/classifiers.py
@@ -2,9 +2,12 @@ import numpy as np
 
 import vigra
 
-from sklearn import svm
-from sklearn.externals import joblib
+from sklearn import ensemble
 from sklearn import naive_bayes
+from sklearn import neighbors
+from sklearn import svm
+
+from sklearn.externals import joblib
 
 import h5py
 
@@ -85,6 +88,52 @@ class GaussianNaiveBayes( Classifier ):
     def __init__( self, **kwargs ):
         self.kwargs = kwargs
         self.classifier = naive_bayes.GaussianNB( **self.kwargs )
+
+    def train( self, data, labels ):
+        self.classifier.fit( data, labels.flat )
+        return self
+
+    def predictLabels( self, data ):
+        return self.classifier.predict( data )[ :, np.newaxis ]
+
+    def predictProbabilities( self, data ):
+        return self.classifier.predict_proba( data )
+
+    def save( self, filePath, pathInFile ):
+        joblib.dump( self.classifier, filePath )
+
+    def load( self, filePath, pathInFile ):
+        self.classifier = joblib.load( filePath )
+
+
+class KNearestNeighbor( Classifier ):
+
+    def __init__( self, **kwargs ):
+        self.kwargs = kwargs
+        self.classifier = neighbors.KNeighborsClassifier( **self.kwargs )
+
+    def train( self, data, labels ):
+        self.classifier.fit( data, labels.flat )
+        return self
+
+    def predictLabels( self, data ):
+        return self.classifier.predict( data )[ :, np.newaxis ]
+
+    def predictProbabilities( self, data ):
+        return self.classifier.predict_proba( data )
+
+    def save( self, filePath, pathInFile ):
+        joblib.dump( self.classifier, filePath )
+
+    def load( self, filePath, pathInFile ):
+        self.classifier = joblib.load( filePath )
+
+
+class AdaBoost( Classifier ):
+
+    def __init__( self, **kwargs ):
+        self.kwargs = kwargs
+        self.classifier = ensemble.AdaBoostClassifier( **self.kwargs )
 
     def train( self, data, labels ):
         self.classifier.fit( data, labels.flat )
