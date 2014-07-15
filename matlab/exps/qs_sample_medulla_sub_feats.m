30f5c62d0afa41be3942d27b356228ed63a6738b
John Bogovic <bogovicj@janelia.hhmi.org>
2014-07-15 10:34:08 -0400

    exp now correctly outputs features for test data

--- matlab/exps/qs_sample_medulla_sub_feats.m
+++ matlab/exps/qs_sample_medulla_sub_feats.m
@@ -76,6 +76,7 @@ watershed_thresh = [0];
 
 %% set up model, do learning
 ts = tic;
+
 dm = dawmr(ds, 3, ec_mlp_h5(mlp_init), script_name);
 dm = dawmr_add_multilayer(dm, dc, [1]);
 dm.learn_features(num_patches_kmeans, 2, [],[],[],50);
@@ -89,17 +90,38 @@ end
 [accs_train, labels_gt_train, ~, labels_pd_train] = ...
     dm.comp_features(1, num_train_instances, ...
                   [],[],[],[], num_workers);
-[accs, labels_gt, ~, labels_pd, aucs] = ...
-    dm.comp_features(3, num_test_instances, ...
-                  [],[],[],[], num_workers);
+              
+if(~isempty(SAVEPATH))
+  save(sprintf('%s/%s_%s_dawmrObj.mat', ...
+               SAVEPATH, SAVEPREFIX, script_name), ...
+       'dm','-v7.3');
+end
+
+% swap image data
+tmp = dm.ds.data_fn{1};
+dm.ds.data_fn{1} = dm.ds.data_fn{3};
+dm.ds.data_fn{3} = tmp;
+
+% swap label data
+tmp = dm.ds.labels_fn{1};
+dm.ds.labels_fn{1} = dm.ds.labels_fn{3};
+dm.ds.labels_fn{3} = tmp;
+
+% swap mask data
+tmp = dm.ds.mask_fn{1};
+dm.ds.mask_fn{1} = dm.ds.mask_fn{3};
+dm.ds.mask_fn{3} = tmp;
+
+[accs_tst, labels_gt_test, ~, labels_pd_test ] = ...
+    dm.comp_features(1, num_test_instances, ...
+                  [],[],[],[], num_workers, 0);
 te = toc(ts);
 
-
 % temporary save
-% if(~isempty(SAVEPATH))
-%   save(sprintf('%s/%s_%s_results.mat', ...
-%                SAVEPATH, SAVEPREFIX, script_name), ...
-%        '-v7.3');
-% end
+if(~isempty(SAVEPATH))
+  save(sprintf('%s/%s_%s_results.mat', ...
+               SAVEPATH, SAVEPREFIX, script_name), ...
+       '-v7.3');
+end
 
 
