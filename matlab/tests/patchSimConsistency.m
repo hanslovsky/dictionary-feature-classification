64059fe0d8e120ea191255c5eef33770476c0a0b
John Bogovic <bogovicj@janelia.hhmi.org>
2014-10-30 16:38:05 -0400

    deeper patch sim consistency tests

--- matlab/tests/patchSimConsistency.m
+++ matlab/tests/patchSimConsistency.m
@@ -12,7 +12,7 @@ if( ~isempty( Dfile ))
     load( Dfile );
 end
 
-D = D(:,1:200);
+D = D(:,1:500);
 
 if( exist( d23File, 'file'))
     load( d23File );
@@ -31,16 +31,58 @@ d23c.clone( d23 );
 d23c.NbestTest = d23c.numDict;
 d23c
 
+%%
+
+d23FastList = cell( numInis, 1 );
+for n = 1:numInis
+    thisobj = d23.copy();
+    thisobj.build3dPatch();
+    d23FastList{n} = thisobj;
+end
+
+%%
+targetDepth = 8;
+
+nodeList = cell( numInis, 1 );
+bestList = cell( numInis, 1 );
+for n = 1:numInis
+    thisobj = d23FastList{n};
+    
+    bestList{n} = thisobj.currentBestPatchParams();
+    
+    it = thisobj.p2dFill3d.getSet().iterator();
+    listout = java.util.ArrayList();
+    while( it.hasNext())
+        listout.add( it.next());
+    end
+    
+    % shuffle the nodes 
+    java.util.Collections.shuffle( listout );
+
+    % pick a matching node matching the target depth
+    it = listout.iterator();
+    while( it.hasNext() )
+        nextNode = it.next();
+        if( nextNode.getDepth() == targetDepth )
+            nodeList{n} = nextNode;
+            break;
+        end
+        
+    end
+end
+
+nodeList
 
 %% compare 'correct' with fast patch similarities
 
-numInis = 1;
+numInis = 10;
 numDict = d23c.numDict;
 numLoc  = 9;
 
 d23List = cell( numInis, 1 );
 
 for n = 1:numInis
+    n
     thisobj = d23c.copy();
     
     [xyzn, iniNode] = thisobj.initializeBuild( );
@@ -60,7 +102,7 @@ end
 
 % d23ListFile = '/groups/saalfeld/home/bogovicj/dev/main/dictionary-feature-classification/matlab/tests/d23TestList.mat';
 % d23ListFile = '/groups/saalfeld/home/bogovicj/dev/main/dictionary-feature-classification/matlab/tests/d23TestList_small.mat';
-d23ListFile = '/groups/saalfeld/home/bogovicj/dev/main/dictionary-feature-classification/matlab/tests/d23TestList_mid.mat';
+d23ListFile = '/groups/saalfeld/home/bogovicj/dev/main/dictionary-feature-classification/matlab/tests/d23TestList_4.mat';
 
 
 if( exist( d23ListFile, 'file' ))
@@ -74,61 +116,124 @@ else
 end
 
 %%
+figdir = '/groups/saalfeld/home/bogovicj/projects/dictionary/dict2dTo3d/patchSim';
 
-leafNode1  = thisobj.pickNextNode( numLoc-1 );
-costsByDepth1 = Dict2dTo3dConsistencyTester.getCostsByDepth( leafNode1, numInis, numLoc );
-costsByDepth1
-
-%
-leafNode2  = thisobj.pickCorrespondingNode( leafNode1 );
-costsByDepth2 = Dict2dTo3dConsistencyTester.getCostsByDepth( leafNode2, numInis, numLoc );
-costsByDepth2
-
-%%
-cmap = diverging_map( linspace(0,1,8), [0.2 0.2 1], [1 0.1 0.1]);
-for i = 1:8
-   
-    plot( costsByDepth1{i}, costsByDepth2{i}, 'o', 'MarkerEdgeColor', cmap(i,:));
+for n = 1:numInis
+    
+    thisobj = d23List{n};
+    
+    leafNode1  = thisobj.pickNextNode( numLoc-1 );
+    costsByDepth1 = Dict2dTo3dConsistencyTester.getCostsByDepth( leafNode1, numInis, numLoc );
+    costsByDepth1
+    
+    %
+    leafNode2  = thisobj.pickCorrespondingNode( leafNode1 );
+    costsByDepth2 = Dict2dTo3dConsistencyTester.getCostsByDepth( leafNode2, numInis, numLoc );
+    costsByDepth2
+    
+    %
+    cmap = diverging_map( linspace(0,1,8), [0.2 0.2 1], [1 0.1 0.1]);
+    for i = 1:8
+        
+        plot( costsByDepth1{i}, costsByDepth2{i}, 'o', 'MarkerEdgeColor', cmap(i,:));
+        hold on;
+    end
+    set(gca,'color',[0.2 0.2 0.2])
+    xlabel( 'patch similarity (correct)');
+    ylabel( 'patch similarity (fast)');
+    
+    % plot identity line
+    axbnds = axis;
+    axisX = axbnds(1:2);
+    axisY = axbnds(3:4);
+    
+    minBnd = min([axisX(1) axisY(1)]);
+    maxBnd = min([axisX(2) axisY(2)]);
+    
     hold on;
+    plot( [minBnd maxBnd], [minBnd maxBnd], '--w' );
+    pause;
+    
+    % export_fig('similarityComparison_small.png', '-m2');
+    export_fig(sprintf('%s/similarityComparison_mid_%d.png', figdir, n), '-m2');
+    close all;
+    
 end
-set(gca,'color',[0.2 0.2 0.2])
-xlabel( 'patch similarity (correct)');
-ylabel( 'patch similarity (fast)');
-
-% plot identity line
-axbnds = axis;
-axisX = axbnds(1:2);
-axisY = axbnds(3:4);
-
-minBnd = min([axisX(1) axisY(1)]);
-maxBnd = min([axisX(2) axisY(2)]);
-
-hold on;
-plot( [minBnd maxBnd], [minBnd maxBnd], '--w' );
-
-% export_fig('similarityComparison_small.png', '-m2');
-export_fig('similarityComparison_mid.png', '-m2');
 
 %%
-% for n = 1:numInis
-%     thisobj = d23List{n};
-%     
-%     filler1 = thisobj.fillObj;
-%     filler2 = thisobj.fillObj2;
-%     
-%     someNode  = thisobj.pickNextNode( numLoc-1 );
-%     someNode2 = thisobj.pickCorrespondingNode( someNode );
-%     
-%     pathToRoot  = someNode.pathToRoot();
-%     pathToRoot2 = someNode2.pathToRoot();
-%     
-%     for d = 1:numLoc
-%         baseNode = pathToRoot.get( d-1 );
-%         children = baseNode.getChildren();
-%         
-%         baseNode2 = pathToRoot2.get( d-1 );
-%         children2 = baseNode2.getChildren();
-%         
-%         
-%     end
-% end
+
+
+%%
+
+load('/groups/saalfeld/home/bogovicj/dev/main/dictionary-feature-classification/matlab/tests/d23TestList_4.mat');
+
+numLoc = 9
+n = 9
+
+%%
+
+wrong = 0;
+right = 0;
+N = 10;
+M = size(thisobj.dimXyzList,1);
+
+rightRankList = zeros( N*M, 1 );
+nn = 1;
+
+testNewer = 1;
+testBest  = 0;
+
+for n = 1:N
+    
+    if( testNewer )
+        thisobj = d23List{n};
+        leafNode  = thisobj.pickNextNode( numLoc-1 );
+    else
+        if( testBest )
+            leafNode = bestList{n};
+        else
+            leafNode = nodeList{n};
+        end
+    end
+    
+    pathToRoot = leafNode.pathToRoot();
+    [ pv, patch, cmtx, b ] = patchFromParams( thisobj, leafNode );
+    
+%     figure; imdisp3d( patch );
+    
+    % see if the 2d-LR projections correspond to patches
+    patchIdxVals = thisobj.leafNodeToPatchIdxs( leafNode );
+    
+
+    for i = 1:M
+        
+        dim = thisobj.dimXyzList( i, 1 );
+        xyz = thisobj.dimXyzList( i, 2 );
+        
+        msk = Dict2dTo3d.planeMaskF( thisobj.sz3d, xyz, dim, thisobj.f );
+        projection = mean( ...
+            reshape(patch( (msk>0) ), thisobj.sz3d ./ [1 1 thisobj.f]),...
+            3);
+        
+        j = patchIdxVals( i, 1 );
+        selectedPatch = reshape(thisobj.D2d( j, : ), thisobj.sz2d);
+        diff = norm( selectedPatch(:) - projection(:) );
+        
+        distancesToDict = pdist2( projection(:)', thisobj.D2d );
+        
+        [~,k] = min(distancesToDict);
+        [ ~, sortedCoords ] = sort( distancesToDict );
+        
+        if( k ~= j )
+            wrong = wrong + 1;
+        else
+            right = right + 1;
+        end
+        
+        rightRankList(nn) = find( sortedCoords == j );
+        nn = nn + 1;
+    end
+end
+
+fprintf('matched %d / %d\n', right, (right+wrong));
+fprintf('in top 5 %d / %d\n', nnz(rightRankList <= 5), (right+wrong));
